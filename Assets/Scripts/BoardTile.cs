c4c442848ec55e245f695b35c72d53f3793b4ddf
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-05-17 20:35:24 +0200

    Add value field to board tile script

--- Assets/Scripts/BoardTile.cs
+++ Assets/Scripts/BoardTile.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoardTile : MonoBehaviour {
+    [SerializeField] private Text valueField;
+
+    private int value = 0;
+
+    public int Value {
+        get { return value; }
+        set {
+            this.value = value;
+
+            if (value != 0) valueField.text = $"{value}";
+            else valueField.text = "";
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.A)) Value++;
 	}
 }
