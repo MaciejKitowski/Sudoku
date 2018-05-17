836a19f428617128a9c496eb644a2a64d0223ee2
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-05-17 21:36:08 +0200

    Add numeric keyboard

---
+++ Assets/Scripts/KeyboardNumeric.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardNumeric : MonoBehaviour {
+    public void NumericButtonPressed(int value) {
+        Debug.Log($"Pressed {value} button");
+    }
+
+    public void ClearButtonPressed() {
+        Debug.Log("Pressed CLEAR button");
+    }
+
+    public void ReturnButtonPressed() {
+        Debug.Log("Pressed RETURN button");
+    }
+}
