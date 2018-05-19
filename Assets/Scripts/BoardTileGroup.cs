d5738d0b1d1c6bf2c74ad72713a3a486b15c6b9c
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-05-19 15:53:55 +0200

    Load tiles to array in BoardTileGroup

---
+++ Assets/Scripts/BoardTileGroup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardTileGroup : MonoBehaviour {
+    private BoardTile[,] tiles = new BoardTile[3, 3];
+
+	void Start () {
+        foreach(Transform tile in transform) {
+            int x = (int)char.GetNumericValue(tile.name[0]);
+            int y = (int)char.GetNumericValue(tile.name[2]);
+
+            tiles[x, y] = tile.GetComponent<BoardTile>();
+        }		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
