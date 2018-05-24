98b5b1242135de450827096ff855930f848077e4
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-05-24 13:19:11 +0200

    Load tile groups in Board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour {
+    private BoardTileGroup[,] tileGroups = new BoardTileGroup[3, 3];
 
-	// Use this for initialization
 	void Start () {
-		
-	}
+        foreach (Transform tile in transform) {
+            int x = (int)char.GetNumericValue(tile.name[0]);
+            int y = (int)char.GetNumericValue(tile.name[2]);
+
+            tileGroups[x, y] = tile.GetComponent<BoardTileGroup>();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
