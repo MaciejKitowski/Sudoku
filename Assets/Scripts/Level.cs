9198bc0b27b3f5cb5573e69cd7111416b998c78e
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-05-29 20:32:14 +0200

    Deserialize matches history

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -16,6 +16,7 @@ public class Level {
         Debug.Log($"Deserialize level from json: {node.ToString()}");
         DeserializeBoard(node);
         DeserializeDisplay(node);
+        DeserializeHistory(node);
 
         Debug.Log("Deserialization finished");
     }
@@ -35,12 +36,48 @@ public class Level {
             }
         }
     }
+
+    private void DeserializeHistory(JSONNode node) {
+        var matches = node["MatchHistory"];
+
+        for (int i = 0; i < matches.Count; ++i) {
+            history.Add(new LevelMatchHistory(matches[i]));
+        }
+
+        Debug.Log($"Deserialized {history.Count} matches");
+    }
 }
 
 public class LevelMatchHistory {
-    private bool wasWon = false;
+    private bool won = false;
     private bool finished = true;
     private int moves = 0;
-    private DateTimeOffset startDate = new DateTimeOffset();
+    private DateTime startDate = new DateTime();
     private int elapsedSeconds = 0;
+
+    public LevelMatchHistory(JSONNode node) {
+        Debug.Log($"Deserialize level match history from json: {node.ToString()}");
+
+        won = node["won"];
+        finished = node["finished"];
+        moves = node["moves"];
+        elapsedSeconds = node["elapsedSeconds"];
+
+        DeserializeStartDate(node);
+    }
+
+    private void DeserializeStartDate(JSONNode node) {
+        string raw = node["date"];
+        var splitted = raw.Split(' ', '-', ':');
+
+        int year, month, day, hour, minutes;
+        if (!int.TryParse(splitted[0], out year)) Debug.LogWarning($"Failed to parse year: {splitted[0]}");
+        if (!int.TryParse(splitted[1], out month)) Debug.LogWarning($"Failed to parse month: {splitted[1]}");
+        if (!int.TryParse(splitted[2], out day)) Debug.LogWarning($"Failed to parse day: {splitted[2]}");
+        if (!int.TryParse(splitted[3], out hour)) Debug.LogWarning($"Failed to parse hour: {splitted[3]}");
+        if (!int.TryParse(splitted[4], out minutes)) Debug.LogWarning($"Failed to parse minutes: {splitted[4]}");
+
+        startDate = new DateTime(year, month, day, hour, minutes, 0);
+        Debug.Log($"Parsed date: {startDate.ToString()}");
+    }
 }
