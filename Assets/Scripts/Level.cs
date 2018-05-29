505b6983b9f99775ce3eba7df05542189687cb44
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-05-29 12:44:55 +0200

    Add Level and LevelMatchHistory classes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Level {
+    private int[,] board = new int[9, 9];
+    private bool[,] displayable = new bool[9, 9];
+    private List<LevelMatchHistory> history = new List<LevelMatchHistory>();
+}
+
+public class LevelMatchHistory {
+    private bool wasWon = false;
+    private bool finished = true;
+    private int moves = 0;
+    DateTimeOffset startDate = new DateTimeOffset();
+    int elapsedSeconds = 0;
+}
