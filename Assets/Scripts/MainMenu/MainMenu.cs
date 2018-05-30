e8886740be295e77e36e9a9fc69ebb8cc4f92590
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-05-30 20:36:28 +0200

    Switch between menus + exit application on escape button

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
+    [SerializeField] private SelectLevel selectLevel = null;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) ButtonExitGame();
+    }
+
+    public void Display() {
+        Debug.Log("Display Main Menu");
+        gameObject.SetActive(true);
+    }
+
+    public void Hide() {
+        Debug.Log("Hide Main Menu");
+        gameObject.SetActive(false);
+    }
 
     public void ButtonSelectLevel() {
         Debug.Log("Pressed Select Level button");
+        selectLevel.Display();
+        this.Hide();
     }
 
     public void ButtonExitGame() {
--- Assets/Scripts/MainMenu/SelectLevel.cs
+++ Assets/Scripts/MainMenu/SelectLevel.cs
@@ -3,6 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectLevel : MonoBehaviour {
+    [SerializeField] private MainMenu mainMenu = null;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) ButtonBackToMenu();
+    }
+
+    public void Display() {
+        Debug.Log("Display Select Level submenu");
+        gameObject.SetActive(true);
+    }
+
+    public void Hide() {
+        Debug.Log("Hide Select Level submenu");
+        gameObject.SetActive(false);
+    }
 
     public void ButtonNewGame() {
         Debug.Log("Pressed New Game button");
@@ -14,5 +29,7 @@ public class SelectLevel : MonoBehaviour {
 
     public void ButtonBackToMenu() {
         Debug.Log("Pressed Back To Main Menu button");
+        mainMenu.Display();
+        this.Hide();
     }
 }
