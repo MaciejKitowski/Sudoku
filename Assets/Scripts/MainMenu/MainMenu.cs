bafd7e01888b3bdf7c3f8bf2dbaeaa4287efc324
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-05-30 16:30:14 +0200

    Add buttons to main menu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public void ButtonSelectLevel() {
+        Debug.Log("Pressed Select Level button");
+    }
+
+    public void ButtonExitGame() {
+        Debug.Log("Pressed Exit Game button");
+        Application.Quit();
+    }
+}
