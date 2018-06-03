4b11c72fdf322029d0f77769bae794117354532f
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-06-03 11:55:24 +0200

    Add Match history item prefab + script

---
+++ Assets/Scripts/MainMenu/MatchHistory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchHistory : MonoBehaviour {
+    [SerializeField] private SelectLevel selectLevel = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Display(Level level) {
+        Debug.Log("Display Match History submenu");
+    }
+
+    public void Hide() {
+        Debug.Log("Hide Match History submenu");
+    }
+
+    public void ButtonBackToMenu() {
+        Debug.Log("Pressed Backu button");
+        selectLevel.Display();
+        this.Hide();
+    }
+}
---
+++ Assets/Scripts/MainMenu/MatchHistoryItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchHistoryItem : MonoBehaviour {
+    [SerializeField] private Text date = null;
+    [SerializeField] private Text moves = null;
+    [SerializeField] private Text time = null;
+
+    public void SetMatch(LevelMatchHistory match) {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
