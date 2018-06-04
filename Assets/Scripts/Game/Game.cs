51fb3345e2309441805580340241e02d8bdd3310
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-06-04 12:12:23 +0200

    Add async timer task

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -1,29 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Timers;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
     [SerializeField] private Board board = null;
+    [SerializeField] private Text timerText = null;
+    [SerializeField] private Text moveCounterText = null;
 
     private LevelManager levelManager = null;
+    private int elapsedSeconds = 0;
+    
+    private int ElapsedSeconds {
+        get { return elapsedSeconds; }
+        set {
+            elapsedSeconds = value;
+            timerText.text = $"{value / 60:D2}:{value % 60:D2}";
+        }
+    }
 
 	void Awake() {
         levelManager = LevelManager.Instance;
         board.BoardFinishedLoading += BoardFinishedLoading;
         board.BoardReadyToPlay += BoardReadyToPlay;
+
+        ElapsedSeconds = 0;
 	}
 
     private void BoardFinishedLoading() {
         Debug.Log("Board finished loading, set level");
 
-        if (levelManager.SelectedLevel == null) Debug.LogError("Selected level is null");
-        else {
-            Debug.Log("Set level on board");
-            board.SetLevel(levelManager.SelectedLevel);
-        }
+        //DEBUG
+        Level lev = levelManager.DifficultLevels[0];
+        board.SetLevel(lev);
+
+        //if (levelManager.SelectedLevel == null) Debug.LogError("Selected level is null");
+        //else {
+        //    Debug.Log("Set level on board");
+        //    board.SetLevel(levelManager.SelectedLevel);
+        //}
     }
 
     private void BoardReadyToPlay() {
         Debug.Log("Board is ready to play");
+
+        StartTimer();
+    }
+
+    private async void StartTimer() {
+        while(true) {
+            await Task.Delay(1000);
+            ElapsedSeconds++;
+        }
     }
 }
