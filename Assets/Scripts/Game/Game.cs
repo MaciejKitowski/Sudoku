64715a0e80755d3ae032fa6be1a0b7b5b5ef0ea8
Maciej Kitowski <MaciejKitowski@hotmail.com>
2018-06-04 11:21:09 +0200

    Add Game class

---
+++ Assets/Scripts/Game/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    [SerializeField] private Board board = null;
+
+	void Start () {
+        board.BoardFinishedLoading += BoardFinishedLoading;
+	}
+
+    private void BoardFinishedLoading() {
+        Debug.Log("Board finished loading, set level on arena");
+    }
+}
